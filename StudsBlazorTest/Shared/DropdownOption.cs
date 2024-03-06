2e60afa06c24861780de21e35fb95939d50e7977
James Zechman <jzechman@strongtie.com>
2024-03-06 09:37:29 -0600

    Initial Commit

---
+++ StudsBlazorTest/Shared/DropdownOption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.Json.Serialization;
+
+namespace StudsBlazorTest.Shared
+{
+	public class DropdownOption
+	{
+        [JsonPropertyName("label")]
+        public string label { get; set; }
+
+        [JsonPropertyName("value")]
+        public string value { get; set; }
+    }
+}
+
